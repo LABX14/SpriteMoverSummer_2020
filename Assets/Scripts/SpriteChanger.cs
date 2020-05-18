5e5392064f96b6850e9afbcd6627bf293f5dcd06
Kaulana Lee <kaulee@uat.edu>
2020-05-18 12:22:00 -0700

    I added in a Space Ship sprite

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        //spriteRenderer.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
